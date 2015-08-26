64653ca4019b723261da69c0ad9821c2d15802f7
Sergio Diniz <sergiodinizsh@hotmail.com>
2015-08-26 13:54:39 -0300

    comecando a fazer o botao de ajudar

--- EGC/src/main/java/com/br/controle/ControladorDenuncia.java
+++ EGC/src/main/java/com/br/controle/ControladorDenuncia.java
@@ -26,6 +26,7 @@ import javax.annotation.PostConstruct;
 public class ControladorDenuncia implements Serializable{
     
     private MapModel simpleModel;
+    private int numero = 0;
   
     
     public MapModel marcaPonto(Denuncia denuncia) throws ParseException {
@@ -68,5 +69,23 @@ public class ControladorDenuncia implements Serializable{
     public MapModel getSimpleModel() {
         return simpleModel;
     }
+    
+    public void ajudarDenuncia(Denuncia denuncia){
+        System.out.println("denuncia: "+denuncia.getDescricao());
+        this.numero++;
+        System.out.println("numero:" + this.numero);
+        
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    
+    
     
 }
