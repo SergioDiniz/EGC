004adcf6eff85a7a3f8ba2280cb13f5924867b45
Sergio Diniz <sergiodinizsh@gmail.com>
2016-02-28 09:40:35 -0300

    configurando email html com commons-email

--- EGC/src/main/java/com/br/controle/ControladorAdmin.java
+++ EGC/src/main/java/com/br/controle/ControladorAdmin.java
@@ -14,12 +14,19 @@ import com.br.fachada.Fachada;
 import java.io.IOException;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
+import org.apache.commons.mail.SimpleEmail;
 
 /**
  *
@@ -33,7 +40,7 @@ public class ControladorAdmin implements Serializable {
     private Prefeitura prefeituraAx;
     private Denuncia denunciaReclamacao;
     private Funcionario gerenciarFuncionario;
-    
+
     private boolean mostrarModalRecusar;
     private boolean mostrarModalAceitar;
 
@@ -157,61 +164,92 @@ public class ControladorAdmin implements Serializable {
 //        FacesContext.getCurrentInstance().getExternalContext().redirect("/EGC/sis/admin/solicitacoes.jsf#modal");
     }
 
-    
-    public int numeroDeFuncionarios(){
+    public int numeroDeFuncionarios() {
         return fachada.numeroDeFuncionarios();
     }
-    
-    public List<Funcionario> todosFuncionarios(){
+
+    public List<Funcionario> todosFuncionarios() {
         return fachada.todosFuncionarios();
     }
-    
-    
-    public List<Denuncia> denunciasComReclamacoes(){
+
+    public List<Denuncia> denunciasComReclamacoes() {
         return fachada.denunciasComReclamacoes();
     }
-    
-    
-    public String selecionarDenunciaReclamacao(Denuncia denuncia){
+
+    public String selecionarDenunciaReclamacao(Denuncia denuncia) {
         this.denunciaReclamacao = new Denuncia();
         this.denunciaReclamacao = denuncia;
 
         return "gerenciar-reclamacao.jsf?faces-redirect=true";
     }
-    
-    public void bloquearDenuncia(Denuncia denuncia){
+
+    public void bloquearDenuncia(Denuncia denuncia) {
         fachada.bloquearDenuncia(denuncia);
         this.denunciaReclamacao.setAtivo(false);
     }
-    
-    public void desbloquearDenuncia(Denuncia denuncia){
+
+    public void desbloquearDenuncia(Denuncia denuncia) {
         fachada.desbloquearDenuncia(denuncia);
         this.denunciaReclamacao.setAtivo(true);
     }
-    
-    
-    public List<ConteudoInapropriado> comentariosDeConteudoInapropriadoEmDenuncia(){
+
+    public List<ConteudoInapropriado> comentariosDeConteudoInapropriadoEmDenuncia() {
         return fachada.comentariosDeConteudoInapropriadoEmDenuncia(this.denunciaReclamacao);
     }
-    
-    public String selecionarFuncionarioGerenciar(Funcionario funcionario){
+
+    public String selecionarFuncionarioGerenciar(Funcionario funcionario) {
         this.gerenciarFuncionario = new Funcionario();
         this.gerenciarFuncionario = funcionario;
         return "perfil-funcionario.jsf?faces-redirect=true";
     }
-    
-   
-    public String atualizarDadosFuncionario(){
+
+    public String atualizarDadosFuncionario() {
         infoDadosAtualizados();
         fachada.atualizar(this.gerenciarFuncionario);
         return "perfil-funcionario.jsf?faces-redirect=true";
     }
-    
-    public List<Prefeitura> prefeiturasDoFuncionario(String email){
+
+    public List<Prefeitura> prefeiturasDoFuncionario(String email) {
         return fachada.prefeiturasDoFuncionario(email);
     }
-    
-    
+
+    public String enviarEmail() throws EmailException, MalformedURLException {
+        System.out.println("Preparando email: ");
+        String eDestino = "sergiodinizsh@gmail.com";
+        String eRemetente = "sergiodinizsh@gmail.com";
+        String eTitulo = "EGC - Eficiência em Gestão de Cidades ";
+        String eMensagem = "Olá, \n Esté é um email de teste";
+
+        HtmlEmail email = new HtmlEmail();
+        email.setHostName("smtp.gmail.com");
+        email.setSmtpPort(587);
+        email.setAuthenticator(new DefaultAuthenticator(eRemetente, "senha"));
+        email.setSSLOnConnect(true);
+        email.setFrom(eRemetente);
+        email.setSubject(eTitulo);
+//        email.setMsg(eMensagem);
+        email.addTo(eDestino);
+        email.setTLS(true);
+        email.setSSL(true);
+
+        // adiciona uma imagem ao corpo da mensagem e retorna seu id
+//        URL url = new URL("http://www.apache.org/images/asf_logo_wide.gif");
+//        String cid = email.embed(url, "Apache logo");
+
+        // configura a mensagem para o formato HTML
+//        email.setHtmlMsg("<html>The apache logo - <img src=\"cid:" + cid + "\"></html>");
+        email.setHtmlMsg("<html> <head> <meta charset=\"UTF-8\"> <style>*{font-family: sans-serif;}</style> </head> <body> <div align=\"center\" style=\"margin-top: 30px;\"> <img src=\"https://raw.githubusercontent.com/SergioDiniz/EGC/master/EGC/src/main/webapp/img/logo-azul.png\" alt=\"Logo EGC\"/> </div><div align=\"center\"> <h2 style=\"font-weight: 100\">Eficiência em Gestão de Cidades</h2> </div><div style=\"color: #fff; margin-top: 20px;padding: 15px 0 15px 0; background-color: #03a4f7; width: 100%; max-width: 600px; margin: 0 auto; text-align: center\"> BEM-VINDO! </div><div style=\"margin: 0 auto; margin-top: 50px; max-width: 500px\" align=\"center\"> Olá <b style=\"color: #03a4f7\">SergioD</b> <br/><br/><br/> Seu cadastrado foi realizado com sucesso e você já pode começar a usar.<br/><br/><br/> <section style=\"margin-bottom: 8px\"><b>Parabéns</b> por se inscrever no <b>EGC</b>! <br/></section> Juntos vamos redefinir o processo de reclamações e criamentos cidades mais inteligentes! </div><div align=\"center\" style=\"margin-top: 40px\"> <img src=\"https://gallery.mailchimp.com/c86887742b453115c5b3a7e0c/images/4c1274c6-cc26-4205-a53c-04f5c5a76c70.png\" width=\"80\" alt=\"Logo EGC\"/> </div><div align=\"center\" style=\"margin-top: 40px; background-color: #f9f9f9; padding: 40px 0 40px 0\"> <div align=\"center\"> <img src=\"https://raw.githubusercontent.com/SergioDiniz/EGC/master/EGC/src/main/webapp/img/s-logo.png\" width=\"80\" alt=\"Logo EGC\"/> </div><br/> <br/><br/> Em caso de <b>dúvidas</b>, entre em contato conosco através do email: <b style=\"color: #03a4f7\">contato@egc.com.br</b> <br/><br/> <i>Copyright © 2016 - EGC, Todos os direitos reservados.</i> </div></body></html>");
+
+        // configure uma mensagem alternativa caso o servidor não suporte HTML
+        email.setTextMsg("Seu servidor de e-mail não suporta mensagem HTML");
+
+        System.out.println("Tentando enviar: ");
+        email.send();
+
+        System.out.println("Email enviado. ");
+        return null;
+    }
+
     //getes and seters
     public Administrador getAdministrador() {
         return administrador;
@@ -260,7 +298,5 @@ public class ControladorAdmin implements Serializable {
     public void setGerenciarFuncionario(Funcionario gerenciarFuncionario) {
         this.gerenciarFuncionario = gerenciarFuncionario;
     }
-    
-    
 
 }
